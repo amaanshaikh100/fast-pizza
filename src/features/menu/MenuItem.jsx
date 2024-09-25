a84da8933a27a82fecae536e440c7440f078af78
YOUR_USERNAME <shaikhamaannn100@gmail.com>
2024-09-25 15:02:23 +0530

    Add delete button component

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -1,12 +1,16 @@
 import { formatCurrency } from "../../utils/helpers";
 import Button from '../../ui/Button';
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { addItem } from "../cart/cartSlice";
+import DeleteItem from "../cart/DeleteItem";
+import { getCurrentQuantityById } from "../cart/cartSlice";
 
 
 function MenuItem({ pizza }) {
   const dispatch = useDispatch();
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
+  const currentQuantity = useSelector(getCurrentQuantityById(id));
+  const isInCart = currentQuantity > 0;
 
   function handleAddToCart() {
     const newItem = 
@@ -29,6 +33,8 @@ function MenuItem({ pizza }) {
         <p className="text-sm italic text-stone-500 capitalize">{ingredients.join(", ")}</p>
         <div className="mt-auto flex items-center justify-between">
           {!soldOut ? <p className="text-sm">{formatCurrency(unitPrice)}</p> : <p className="text-sm font-medium uppercase text-stone-500">Sold out</p>}
+
+          {isInCart && <DeleteItem pizzaId={id} />}
 
           {!soldOut && <Button type="small" onClick={handleAddToCart}>Add to cart</Button>}
         </div>
